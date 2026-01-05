a650bfa33f84cd27f89004774069f9a9362dd115
luciD <sosalnet@yandex.ru>
2026-01-05 03:43:06 +0300

    added y metrika

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,10 @@ import '../../public/fonts/fonts.css';
 import '../../public/normalize.css';
 import '../globals.scss';
 import Script from 'next/script';
+import { useYandexMetrikaRouter } from '@/hooks/useYandexMetrika';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useYandexMetrikaRouter();
     // TODO проверить какие правильный иконки сюда нужно ставить, в public/icons есть файл site.webmanifest. может быть лучше подключить его?
     return (
         <Layout>
@@ -28,6 +30,32 @@ function MyApp({ Component, pageProps }: AppProps) {
                 rel="stylesheet"
                 href="https://widget.prodamus.ru/src/init.css"
             />
+            {/* Yandex.Metrika counter */}
+            <Script
+                id="yandex-metrika"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(m,e,t,r,i,k,a){
+                            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106109589', 'ym');
+                        ym(106109589, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+                    `,
+                }}
+            />
+            <noscript>
+                <div>
+                    <img
+                        src="https://mc.yandex.ru/watch/106109589"
+                        style={{ position: 'absolute', left: '-9999px' }}
+                        alt=""
+                    />
+                </div>
+            </noscript>
+            {/* /Yandex.Metrika counter */}
             <Component {...pageProps} />
         </Layout>
     );
